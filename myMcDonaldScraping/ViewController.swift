307755e98a48baae31022bb1794179f5021967a7
Satochaaan <k97satosi327@gmail.com>
2021-07-29 08:29:48 +0900

    initial commit.

---
+++ myMcDonaldScraping/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myMcDonaldScraping
+//
+//  Created by 川野智史 on 2021/07/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
